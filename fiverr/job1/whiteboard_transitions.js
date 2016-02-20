9a10c74b7e1921808a14a395b990da30f1f2449c
Mauricio Machuca <mauricio208@gmail.com>
2016-02-19 22:22:22 -0430

    al dia

--- fiverr/job1/whiteboard_transitions.js
+++ fiverr/job1/whiteboard_transitions.js
@@ -3,6 +3,15 @@
 
 // Simple transition functions
 
+function runAnimations(transit_info) {
+	var transit_info=JSON.parse(transit_info);
+	$.each(transit_info,function(k,v){
+		if (k=="mR") {
+			moveRight(v);
+		}
+	})
+}
+
 function moveRight(obj,start,duration,px) {
     if (px>=1) {
         setTimeout( ($(obj).transition({ x:px,duration:duration})) , start*1000)
