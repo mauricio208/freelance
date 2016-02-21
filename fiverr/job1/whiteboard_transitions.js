36dadce2d56d773dff855c7701ed24eb0a17c924
Mauricio Machuca <mauricio208@gmail.com>
2016-02-21 10:29:55 -0430

    adelanto logica menu+

--- fiverr/job1/whiteboard_transitions.js
+++ fiverr/job1/whiteboard_transitions.js
@@ -2,24 +2,48 @@
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //Logic for transition menu
-function loadTransitionInMenu(obj) {
+	function loadTransitionInMenu(obj) {
 		obj.find(".transition").each(function() {
-			mt=$("<div class='menu_transition'></div>");
-			mt.append($(this).clone());
+			mt=$("<div class='menu_transition'><button class='update-animation-transit'>update animation</button></div>");
+			t=$(this).clone();
+			t.prop("class","transition_to_update");
+			mt.append(t);
 			params=$("<div class='transition_group_span_input'></div>");
 			param=$(this).html().split("(")[1].split(")")[0].split(",");
 			param=param.slice(1);
 			for (var i = param.length - 1; i >= 0; i--) {
 				p=param[i].split("=");
-				input=$("<input class='transition_input' type='number' />");
+				input=$("<input class='transition_input' type='number' name='"+p[0]+"' />");
 				input.val(p[1]);
-				params.append("<span class='transition_span_input'>"+p[0]+"</span>").append(input);
+				span=$("<span class='transition_span_input'>"+p[0]+"</span>").append(input);
+				params.append(span);
 			}
 			mt.append(params);
 			obj.find(".menu_transitions").append(mt)
 		});
 	}
-	// function updateTransition(obj)
+	function updateTransitionInMenu(obj){
+		t=obj.find(".transition_to_update");
+		function_name=t.html().split("(")[0];
+		params="";
+		obj.find(".transition_input").each(function(){
+			params=","+$(this).attr("name")+"="+$(this).val()+params;
+		})
+		updated_function=function_name+"(obj"+params+")";
+		t.html(updated_function);
+	};
+
+	function updateTransitionToComponent(obj){
+		scripts=obj.find("script");
+		if (scripts.length > 0){
+			scripts.each(function(){
+				$(this).prop("class","transition");
+			});
+			t=obj.parent().find(".transitions")
+			t.html("");
+			t.append(scripts);
+		}
+	};
 	$(document).mouseup(function (e)
 	{
 	    var containers = $(".menu_manage_transitions");
@@ -29,6 +53,7 @@ function loadTransitionInMenu(obj) {
 		        && container.has(e.target).length === 0) 
 		    {
 		        container.hide();
+		        updateTransitionToComponent(container);
 		    }
 
 	    });
@@ -38,16 +63,23 @@ function loadTransitionInMenu(obj) {
 	$(".component").on("click",function (){
 		menu=$(this).find(".menu_manage_transitions");
 		if ( menu.length == 0) {
-			$(this).append("<div class='menu_manage_transitions' ><button class='add-animation-transit'>Add animation</button><div class='menu_transitions'></div></div>");
+			$(this).append("<div class='menu_manage_transitions'><button class='add-animation-transit'>Add animation</button><div class='menu_transitions'></div></div>");
 		}else{
 			menu.find(".menu_transitions").html("");
 		};
 		menu.show();
 		loadTransitionInMenu($(this));
+	}).on('click','.menu_manage_transitions',function(e) {
+        e.stopPropagation();
+    }).on('click',".update-animation-transit",function() {
+		updateTransitionInMenu($(this).parent());
 	});
-	$("#add-animation-transit").on("",function() {
+
+	$(".add-animation-transit").on("click",function() {
 		
 	});
+	
+
 
 // Simple transition functions
 function runMyTransitions(obj) {
