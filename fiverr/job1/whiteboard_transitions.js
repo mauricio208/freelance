8486e670d209fd8cd0e2ce821d2784b76e371fff
Mauricio Machuca <mauricio208@gmail.com>
2016-02-22 14:06:15 -0430

    git pull

--- fiverr/job1/whiteboard_transitions.js
+++ fiverr/job1/whiteboard_transitions.js
@@ -2,26 +2,32 @@
 // --------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //Logic for transition menu
-	function loadTransitionInMenu(obj) {
-		obj.find(".transition").each(function() {
-			mt=$("<div class='menu_transition'><button class='update-animation-transit'>update animation</button></div>");
-			t=$(this).clone();
-			t.prop("class","transition_to_update");
-			mt.append(t);
-			params=$("<div class='transition_group_span_input'></div>");
-			param=$(this).html().split("(")[1].split(")")[0].split(",");
-			param=param.slice(1);
-			for (var i = param.length - 1; i >= 0; i--) {
+
+	function createInputsInMenu(contenedor,transition_element) {
+		param=transition_element.split("(")[1].split(")")[0].split(",");
+		param=param.slice(1);
+		for (var i = param.length - 1; i >= 0; i--) {
 				p=param[i].split("=");
 				input=$("<input class='transition_input' type='number' name='"+p[0]+"' />");
 				input.val(p[1]);
 				span=$("<span class='transition_span_input'>"+p[0]+"</span>").append(input);
-				params.append(span);
+				contenedor.append(span);
 			}
+	}
+
+
+	function loadTransitionInMenu(obj) {
+		obj.find(".transition").each(function() {
+			mt=$("<div class='menu_transition'><button type='button' class='update-animation-transit'>update animation</button></div>");
+			t=$(this).clone();
+			t.prop("class","transition_to_update");
+			mt.append(t);
+			params=$("<div class='transition_group_span_input' hidden></div>");
+			createInputsInMenu(params,$(this).html());
 			mt.append(params);
 			obj.find(".menu_transitions").append(mt)
 		});
-	}
+	};
 	function updateTransitionInMenu(obj){
 		t=obj.find(".transition_to_update");
 		function_name=t.html().split("(")[0];
@@ -34,7 +40,7 @@
 	};
 
 	function updateTransitionToComponent(obj){
-		scripts=obj.find("script");
+		scripts=obj.find(".transition_to_update");
 		if (scripts.length > 0){
 			scripts.each(function(){
 				$(this).prop("class","transition");
@@ -44,64 +50,91 @@
 			t.append(scripts);
 		}
 	};
-	$(document).mouseup(function (e)
-	{
-	    var containers = $(".menu_manage_transitions");
-	    containers.each(function(){
-	    	container=$(this);
-		    if (!container.is(e.target) 
-		        && container.has(e.target).length === 0) 
-		    {
-		        container.hide();
-		        updateTransitionToComponent(container);
-		    }
+	function addTransitionToComponent(obj) {
 
-	    });
-	    
+		component=obj.closest(".component");
+
+		t=obj.parent();
+		function_name=obj.prop("name").split("(")[0];
+		params="";
+		t.find(".transition_input").each(function(){
+			params=","+$(this).attr("name")+"="+$(this).val()+params;
+		})
+		func=function_name+"(obj"+params+")";
+		span=$("<span class='transition' hidden>");
+		span.html(func);
+		component.find(".transitions").append(span);
+		t.hide();
+		t.parent().hide();
+	};
+	$(document).ready(function(){
+		$(document).mouseup(function (e)
+		{
+		    var containers = $(".menu_manage_transitions");
+		    containers.each(function(){
+		    	container=$(this);
+			    if (!container.is(e.target) 
+			        && container.has(e.target).length === 0) 
+			    {
+			        container.hide();
+			        updateTransitionToComponent(container);
+			    }
+
+		    });
+		    
+		});
+
+		$(".component").on("click",function (){
+			menu=$(this).find(".menu_manage_transitions");
+			if ( menu.length == 0) {
+				$(this).append("<div class='menu_manage_transitions'><button type='button' class='add-animation-transit'>Add animation</button><div class='menu_transitions'></div></div>");
+			}else{
+				menu.find(".menu_transitions").html("");
+			};
+			menu.show();
+			loadTransitionInMenu($(this));
+		}).on('click','.menu_manage_transitions',function(e) {
+	        e.stopPropagation();
+	    }).on('click',".update-animation-transit",function() {
+			updateTransitionInMenu($(this).parent());
+		}).on('click',".add-animation-transit",function() {
+			if ($(this).siblings(".transitions_to_add").) {}
+			$(this).siblings(".transitions_to_add").show();
+		}).on('click',".add_transition_button",function() {
+			t=$(".param_transition_to_add");
+			t.find(".input_transitions_to_add").html("");
+			createInputsInMenu(t.find(".input_transitions_to_add"),$(this).prop("name"));
+			t.find(".add_transition").prop("name",$(this).prop("name"));
+			t.show();
+			
+		}).on('click',".add_transition",function() {
+			addTransitionToComponent($(this));
+		});
+
 	});
 
-	$(".component").on("click",function (){
-		menu=$(this).find(".menu_manage_transitions");
-		if ( menu.length == 0) {
-			$(this).append("<div class='menu_manage_transitions'><button class='add-animation-transit'>Add animation</button><div class='menu_transitions'></div></div>");
-		}else{
-			menu.find(".menu_transitions").html("");
-		};
-		menu.show();
-		loadTransitionInMenu($(this));
-	}).on('click','.menu_manage_transitions',function(e) {
-        e.stopPropagation();
-    }).on('click',".update-animation-transit",function() {
-		updateTransitionInMenu($(this).parent());
-	});
 
-	$(".add-animation-transit").on("click",function() {
-		
-	});
-	
-
-
-// Simple transition functions
+// Transition functions
 function runMyTransitions(obj) {
-	$(".component").find("script.transit").each(functon(){
-		eval(this.html());
+	obj.find(".transition").each(function(){
+		eval($(this).html());
 	});
 };
 
 function runAllTransitions() {
 	$(".component").each(function(){
-		runMyTransitions(this);
+		runMyTransitions($(this));
 	});
 };
 
 
 function moveRight(obj,start,duration,px) {
     if (px>=1) {
-        setTimeout( ($(obj).transition({ x:px,duration:duration})) , start*1000)
+        $(obj).velocity({ translateX:"+="+String(px)},{duration:duration*1000,delay:start*1000})
     }
 };
 function moveLeft(obj,start,duration,px) {
-    if (px<0) {
-        setTimeout( ($(obj).transition({ x:px,duration:duration})) , start*1000)
+    if (px>=1) {
+    	$(obj).velocity({ translateX:"+="+String(px*-1)},{duration:duration*1000,delay:start*1000})
     }
 };
